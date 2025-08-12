26e843795dc48900e75226a58779aa64e47689ee
Miguel Leirião <miguel.leiriao@hotmail.com>
2025-08-12 23:43:46 +0100

    chore: initial commit (landing + tailwind setup + license)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./*.html", "./**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#ff7a00", // laranja principal
+          dark: "#cc6200",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
